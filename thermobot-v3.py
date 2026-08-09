c6c05d484d0349898f5dba053449fff46ba9948c
ambreww12 <ass7711are@gmail.com>
2026-08-09 19:07:20 -0400

    Update thermobot-v3.py

--- thermobot-v3.py
+++ thermobot-v3.py
@@ -6,10 +6,13 @@
 #   ███████║   ██║   ╚██████╔╝██║     ██╗██╗
 #   ╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝
 # STOP!!
-# NONE OF THIS CODE IS LICENSABLE.
+# -------------------------------------------------------
+# PUBLICLY AVAILABLE CODE ≠ LICENSABLE/FREE TO USE.
+# THIS CODE IS NOT LICENSABLE.
 # USING WITHOUT PERMISSION CONSTITUTES COPYRIGHT INFRINGEMENT, PUNISHABLE BY STATUATORY DAMAGES OF $750 TO $30,000 ($150,000 IF WILLFUL).
-# You wanna risk the cost of a car over a discord bot? Yeah thats what I thought.
+# You wanna risk the cost of a car over a discord bot? Yeah, that's what I thought.
 # YOU HAVE BEEN WARNED!!!
+# -------------------------------------------------------
 
 disable_thermoquestions = True
 
